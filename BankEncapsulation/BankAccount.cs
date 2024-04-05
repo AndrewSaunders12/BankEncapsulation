3cb0b49c06e490b3a3ebc51028526f10889b2498
AndrewSaunders12 <ninjaraider04@gmail.com>
2024-04-04 22:06:37 -0400

    Money Bank

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double _balance = 0;
+
+        public double Deposit()
+        {
+
+            Console.WriteLine("How much do you want to deposit");
+            var amount = double.Parse(Console.ReadLine());
+            Console.WriteLine($"_balance + {amount}");
+            _balance = _balance + amount;
+            Console.WriteLine(_balance);
+            return _balance;
+        }
+
+        public double GetBalance()
+        {
+            Console.WriteLine($"Balance; {_balance}");
+            return _balance;
+        }
+
+        public double Withdraw()
+        {
+            Console.WriteLine("How much do you want to withdraw");
+            var amount = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Withdrawl Amount: -{amount}");
+            _balance = _balance - amount;
+            Console.WriteLine(_balance);
+            return _balance;
+        }
+
+        public bool Pin()
+        {
+
+            Console.WriteLine("Please enter your pin");
+            var number = int.Parse(Console.ReadLine());
+
+            if (number == 4321)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Wrong Pin");
+                Pin();
+                return false;
+            }
+
+
+
+        }
+
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+            Console.WriteLine("How would you like to continue /Deposit /Withdraw /Check Balance");
+            string userInput = Console.ReadLine();
+            account.Pin();
+
+
+
+            if (userInput == "Balance")
+            {
+                account.GetBalance();
+            }
+            if (userInput == "Withdraw")
+            {
+                account.Withdraw(); 
+            }
+            if (userInput == "Deposit")
+            {
+                account.Deposit();
+            }
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
