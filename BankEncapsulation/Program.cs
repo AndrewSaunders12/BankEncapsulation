7519908da7cd2fb74b8303d015627e4391f22063
AndrewSaunders12 <ninjaraider04@gmail.com>
2024-11-07 18:56:29 -0500

    Encapsulating

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace BankEncapsulation
+﻿using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+            Console.WriteLine("How much do you want to Deposit?");
+            var depoAmount = double.Parse(Console.ReadLine());
+            account.Deposit(depoAmount);
+            Console.WriteLine($"Your new balance is {account.GetBalance()}");
         }
     }
 }
